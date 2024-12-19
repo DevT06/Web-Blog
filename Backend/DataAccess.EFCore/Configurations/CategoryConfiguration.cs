70fc89520124641688363344a2c30000a3624b5d
DevT06 <timothygmaurer@outlook.com>
2024-12-19 13:11:28 +0100

    Update CategoryEntries

--- Backend/DataAccess.EFCore/Configurations/CategoryConfiguration.cs
+++ Backend/DataAccess.EFCore/Configurations/CategoryConfiguration.cs
@@ -22,17 +22,18 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 
         builder.HasData(new List<Category>
         {
-            new Category { Id = CategoryEnum.Travel,Name = "Travel" },
-            new Category { Id = CategoryEnum.Holiday, Name = "Holiday" },
-            new Category { Id = CategoryEnum.Car, Name = "Car" },
-            new Category { Id = CategoryEnum.Beauty, Name = "Beauty" },
-            new Category { Id = CategoryEnum.Country, Name = "Country" },
-            new Category { Id = CategoryEnum.Hiking, Name = "Hiking" },
-            new Category { Id = CategoryEnum.Sports, Name = "Sports" },
-            new Category { Id = CategoryEnum.Life, Name = "Life" },
-            new Category { Id = CategoryEnum.Education, Name = "Education" },
-            new Category { Id = CategoryEnum.Achievement, Name = "Achievement" },
-            new Category { Id = CategoryEnum.Story, Name = "Story" }
+	        new Category { Id = CategoryEnum.Travel, Name = "Travel", Description = "Exploring new destinations, cultures, and experiences around the world." },
+	        new Category { Id = CategoryEnum.Holiday, Name = "Holiday", Description = "Special occasions and celebrations, often marked by leisure and joy." },
+	        new Category { Id = CategoryEnum.Car, Name = "Car", Description = "Everything about cars, from models and features to maintenance and reviews." },
+	        new Category { Id = CategoryEnum.Beauty, Name = "Beauty", Description = "Topics about skincare, makeup, fashion, and enhancing personal appearance." },
+	        new Category { Id = CategoryEnum.Country, Name = "Country", Description = "Insights and details about nations, their cultures, and landmarks." },
+	        new Category { Id = CategoryEnum.Hiking, Name = "Hiking", Description = "Adventures in nature, including trails, mountains, and outdoor exploration." },
+	        new Category { Id = CategoryEnum.Sports, Name = "Sports", Description = "Activities and competitions that involve physical effort and skill." },
+	        new Category { Id = CategoryEnum.Life, Name = "Life", Description = "Reflections and insights on everyday living and personal experiences." },
+	        new Category { Id = CategoryEnum.Education, Name = "Education", Description = "Learning opportunities, academic subjects, and personal growth." },
+	        new Category { Id = CategoryEnum.Achievement, Name = "Achievement", Description = "Milestones and accomplishments in personal or professional life." },
+	        new Category { Id = CategoryEnum.Story, Name = "Story", Description = "Narratives and accounts of experiences, real or fictional." }
+
 		});
     }
 }
